bf69ee45e9b148d1eb10be5bd6fd43e1aad61a45
Loka <loka@iim.ai>
2024-09-13 17:03:09 +0800

    completion_tokens_details

--- common.go
+++ common.go
@@ -4,7 +4,10 @@ package openai
 
 // Usage Represents the total token usage per request to OpenAI.
 type Usage struct {
-	PromptTokens     int `json:"prompt_tokens"`
-	CompletionTokens int `json:"completion_tokens"`
-	TotalTokens      int `json:"total_tokens"`
+	PromptTokens            int `json:"prompt_tokens"`
+	CompletionTokens        int `json:"completion_tokens"`
+	TotalTokens             int `json:"total_tokens"`
+	CompletionTokensDetails struct {
+		ReasoningTokens int `json:"reasoning_tokens"`
+	} `json:"completion_tokens_details"`
 }
